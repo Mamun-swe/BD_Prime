574a3525ccdeeed8eec5ef3847f8f32dd5750016
Abdullah Al Mamun <mhmamun166009@gmail.com>
2020-10-26 02:27:44 +0600

    Message Room Added

--- src/components/FourOFour/Index.js
+++ src/components/FourOFour/Index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import '../../App.scss'
 import FourOFourImg from '../../assets/static/notfound.png'
 
-const Index = () => {
+const Index = ({ messages }) => {
     return (
         <div className="text-center fourOfour-box">
             <img src={FourOFourImg} className="img-fluid" alt="..." />
-            <h6>No videos found</h6>
+            {messages ? <h6>{messages}</h6> :
+                <h6>No videos found</h6>
+            }
         </div>
     );
 };
--- src/pages/user/chat/Master.js
+++ src/pages/user/chat/Master.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom'
 
 import Messages from './Messages'
 import Peoples from './Peoples'
+import MessageRoom from './MessageRoom'
 
 const Master = () => {
     return (
@@ -10,6 +11,7 @@ const Master = () => {
             <Route>
                 <Route exact path="/home/chat/" component={Messages} />
                 <Route exact path="/home/chat/peoples" component={Peoples} />
+                <Route path="/home/chat/messages/:name/:id" component={MessageRoom} />
             </Route>
         </div>
     );
